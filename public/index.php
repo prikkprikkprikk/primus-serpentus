9dae8d1b38d29a6539a855260b05e3072bd36616
Jørn Støylen <jorn@digitaltwist.no>
2022-02-27 11:38:11 +0100

    Add APP_ROOT constant

--- public/index.php
+++ public/index.php
@@ -2,10 +2,12 @@
 
 namespace App;
 
+use App\ApiController;
+
 require_once('../vendor/autoload.php');
 require_once('../src/config.php');
 
-use App\ApiController;
+define('APP_ROOT', dirname(__FILE__,2));
 
 $requestUri = strtok($_SERVER['REQUEST_URI'], '?');
 
